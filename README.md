6b32814f48d07fdd05ac5d516856d797d549f734
Boris Gubanov <boris.gu.98@gmail.com>
2023-04-21 15:28:07 +0300

    Update README and example

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # ArduCRSF-RH
 Arduino library for communicating with external module via CRSF protocol (just like radio handset do)
 
-## WARNING!!! FOR SOME UNKNOWN REASON, CHANNEL 5 (IF YOU START THE COUNT AT 0) MUST BE LESS THAN 512, OR THE PACKET WILL NOT BE RECEIVED.
+## Wiring Diagram 
+ESP32 -> Inverter -> ELRS TX  
+![ELRS_base](/img/ELRS_base.png)
+
+## SoftSerial
+When using SoftSerial it is possible that data transmission errors occur.  
+Known bug: for some unknown reason channel 5 (if starting from 0) must be less than 512, otherwise the packet will not be received.
+
+The maximum baud rate is limited to 115200
--- examples/transmit_packets/main.cpp
+++ examples/transmit_packets/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 #include "arducrsf_rh.h"
+
+// =============================================================
+// SoftSerial
+// When using softserial, data transmission errors are possible
+// and the maximum baud rate is limited to 115200
+// =============================================================
+/*
+#include <SoftwareSerial.h>
 
 #define RX_PIN 4
 #define TX_PIN 5
 
+uint16_t step;
 SoftwareSerial Serial_my(RX_PIN, TX_PIN, true);
 CRSF_RH elrs;
 crsf_rc_channels channels;
@@ -22,33 +30,56 @@ void setup()
     }
 
     elrs.set_port(&Serial_my);
+    step = 1;
     for (uint8_t i = 0; i < 16; i++)
     {
         channels.ch[i] = 173;
     }
 }
+*/
+//=====
+// END
+//=====
+
+//========================
+// 400k with TBS inverter
+//========================
+uint16_t step;
+CRSF_RH elrs;
+crsf_rc_channels channels;
+
+void setup()
+{
+    // USB
+    Serial.begin(460800);
+    // ELRS
+    Serial2.begin(400000);
+    while (!Serial2)
+    {
+        delay(5);
+    }
+
+    elrs.set_port(&Serial2);
+    step = 1;
+    for (uint8_t i = 0; i < 16; i++)
+    {
+        channels.ch[i] = 173;
+    }
+}
+//=====
+// END
+//=====
 
 void loop()
 {
-    if (channels.ch[0] > 1811)
+    if (channels.ch[0] < 173 || channels.ch[0] > 1811)
     {
-        for (uint8_t i = 0; i < 16; i++)
-        {
-            if (i == 5)
-            {
-                continue;
-            }
-            channels.ch[i] = 173;
-        }
+        step *= -1;
     }
     elrs.send_rc_channels(channels);
     delay(2);
     for (uint8_t i = 0; i < 16; i++)
     {
-        if (i == 5)
-        {
-            continue;
-        }
-        channels.ch[i] = channels.ch[i] + 1;
+        channels.ch[i] = channels.ch[i] + step;
     }
 }
